883c046ac16ebeb469453302b572cf62b3fbc8c0
Adam Moore <redndahead@gmail.com>
2015-05-15 11:30:46 -0700

    WP-5353: Bunch o changes to try to make things consistent.

--- themes/resource-soundcloud.tpl.php
+++ themes/resource-soundcloud.tpl.php
@@ -1,23 +1,19 @@
 <?php
 
 /**
- * @file resources/themes/media-soundcloud.tpl.php
+ * @file resources/themes/resource-soundcloud.tpl.php
  *
  * Template file for theme('media_soundcloud').
  *
  * Variables available:
  *  $uri - The media uri for the SoundCloud audio (e.g., soundcloud://v/xsy7x8c9).
- *  $video_id - The unique identifier of the SoundCloud audio (e.g., xsy7x8c9).
+ *  $track_id - The unique identifier of the SoundCloud audio (e.g., xsy7x8c9).
  *  $id - The file entity ID (fid).
  *  $url - The full url including query options for the SoundCloud iframe.
- *  $options - An array containing the Media SoundCloud formatter options.
- *  $api_id_attribute - An id attribute if the Javascript API is enabled;
- *  otherwise NULL.
+ *  $options - An array containing the SoundCloud formatter options.
  *  $width - The width value set in Media: SoundCloud file display options.
  *  $height - The height value set in Media: SoundCloud file display options.
- *  $title - The Media: SoundCloud file's title.
- *  $alternative_content - Text to display for browsers that don't support
- *  iframes.
+ *  $title - SoundCloud file's title.
  *
  */
 
