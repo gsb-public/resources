bbc3cf9e58317530f6fff3ac6808ee3a4c4afd32
Greg Mercer (GBear) <gmercer@stanford.edu>
2019-07-03 08:30:42 -0700

    WP-9167 Cleaning up some of the code.

--- themes/resource-infogram.tpl.php
+++ themes/resource-infogram.tpl.php
@@ -6,8 +6,7 @@
  * Template file for theme('media_infogram').
  *
  * Variables available:
- *  $uri - The media uri for the SoundCloud audio (e.g., infogram://v/xsy7x8c9).
- *  $title - Infogram file's title.
+ *  $code - embedded code used for an infogram
  *
  */
 
