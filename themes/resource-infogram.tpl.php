80c41ac475cc65ef8e5b443e5f58d08711cd302b
Greg Mercer (GBear) <gmercer@stanford.edu>
2019-07-03 08:10:58 -0700

    WP-9167 Adding new resource: infogram

---
+++ themes/resource-infogram.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file resources/themes/resource-infogram.tpl.php
+ *
+ * Template file for theme('media_infogram').
+ *
+ * Variables available:
+ *  $uri - The media uri for the SoundCloud audio (e.g., infogram://v/xsy7x8c9).
+ *  $title - Infogram file's title.
+ *
+ */
+
+?>
+<div>
+    <?php print $code; ?>
+</div>
