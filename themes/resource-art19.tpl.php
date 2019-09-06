a7f460ed40e1c8a807d43f23261dfb74412f9ed4
justinriddiough <justinriddiough@stanford.edu>
2019-09-06 03:14:57 -0700

    WP-9196 Resource formatters

---
+++ themes/resource-art19.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file resources/themes/resource-art19.tpl.php
+ *
+ * Template file for theme('media_infogram').
+ *
+ * Variables available:
+ *  $code - embedded code used for an Art19 embed
+ *
+ */
+
+?>
+<div>
+    <?php print $code; ?>
+</div>
